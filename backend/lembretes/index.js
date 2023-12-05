2c2a0d221bcbca12e77dd4317c09ca946e143de8
matheus lobo <Matheus>
2023-12-05 20:48:24 -0300

    ve ai yago

---
+++ backend/lembretes/index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mysql = require('mysql2');
+contador = 0;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'schema1',
+    password: 'melo2020'
+});
+
+app.get('/lembretes/', (req, res) => {
+    connection.query('SELECT * FROM tb_lembretes', (err, results, fields) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Erro interno do servidor');
+            return;
+        }
+        res.send(results);
+    });
+});
+
+app.post('/lembretes/', (req, res) => {
+    contador++;
+    const nome = req.body.texto;
+    const dt = req.body.dt;
+    const concluido = req.body.concluido === 'on' ? true : false;
+    const cor = req.body.cor;
+    const categoria = req.body.categoria;
+
+    const sql = `INSERT INTO tb_lembretes (id, nome, dt, categoria, concluido, cor) VALUES (?, ?, ?, ?, ?, ?)`;
+    const values = [contador, nome, dt, categoria, concluido, cor];
+
+    connection.query(sql, values, (err, results, fields) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Erro interno do servidor');
+            return;
+        }
+        res.status(201).send(results);
+    });
+});
+
+app.listen(4000, () => {
+    console.log('Lembretes. Porta 4000');
+});
---
+++ backend/observacoes/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const { v4: uuidv4 } = require('uuid');
+const axios = require('axios');
+const cors = require('cors');
+const app = express();
+app.use(express.json());
+app.use(cors());
+const mysql = require('mysql2');
+
+const connection2 = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'schema2',
+    password: 'melo2020'
+});
+
+//const observacoesPorLembreteId = {};
+
+app.post('/lembretes/:id/observacoes', async (req, res) => {
+        const id = req.body.id
+        const id_obs = req.body.id_obs
+        const obs = req.body.obs
+        const sql = "INSERT INTO tb_observacoes (id, id_obs, obs) VALUES ("
+        + id + ", '" + id_obs + ", '" + obs + ")"
+        connection.query(sql, (err, results, fields) => {
+        console.log (results)
+        console.log(fields)
+         res.send('ok')
+         })
+        })
+
+app.get('/lembretes/:id/observacoes', (req, res) => {
+    connection.query('SELECT * FROM tb_observacoes', (err, results, fields) => {
+        res.json(results)
+    })
+})
+
+app.listen(5000, (() => {
+    console.log('Observacoes. Porta 5000');
+}));
